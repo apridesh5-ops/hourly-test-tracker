64031a2da2f0e9d9e0637866f57e315d05f3364f
GokulAjithV <gokulvasu225@gmail.com>
2025-10-12 00:45:25 +0530

    fetch-csvs: setup cross-origin resource sharing (cors)

--- file-fetching-server/src/index.ts
+++ file-fetching-server/src/index.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 
 const PORT = 3000;
 
@@ -30,12 +37,14 @@ app.post("/fetch-csvs", async (req, res) => {
 
     console.log("Request body params :", req.body);
 
-    if (!isArray(paths) || paths.length == 0) {
+    if (!isArray(paths) || paths.length === 0) {
         return res.status(400).json({ error: "Please provide atleast one path"});
     }
 
+    const validPaths = paths.filter(path => !!path);
+
     try {
-        const allCsvPromises = paths.map(parseCSV);
+        const allCsvPromises = validPaths.map(parseCSV);
         const allDataArrays = await Promise.all(allCsvPromises);
         const mergedData = allDataArrays.flat();
         const successMessage = "CSV files merged successfully"
