16c1679d34e0825ad8f768bd27423804688ba635
GokulAjithV <gokulvasu225@gmail.com>
2025-11-07 00:06:23 +0530

    common: commonize types

---
+++ hourly-test-client/src/components/common/types.ts
@@ -0,0 +1,19 @@
+export interface CSVRow {
+    Date?: string;
+    Tester_Location?: string;
+    Tester_Result?: string;
+    NG_Qty?: string;
+    TesterID?: string;
+    Carrier_SN?: string;
+    DUT_SN?: string;
+    Error_Code?: string;
+    Error_Content?: string;
+    TesterIP?: string;
+    Project_Name?: string;
+    Script_Name?: string;
+    Serial_Number_Name?: string;
+    Tester_Duration?: string;
+    Tester_Start_Time?: string;
+    Tester_End_Time?: string;
+    Record_Time?: string;
+}
--- hourly-test-client/src/services/api.ts
+++ hourly-test-client/src/services/api.ts
@@ -1,3 +1,4 @@
+import { type CSVRow } from "../components/common/types";
 
 const base_url = 'http://localhost:3000';
 
@@ -13,26 +14,6 @@ interface ProductionRequest {
     endTime?: string;
     testId?: string;
     shift?: 'A' | 'B' | 'C' | '';
-}
-
-interface CSVRow {
-    Date?: string;
-    Tester_Location?: string;
-    Tester_Result?: string;
-    NG_Qty?: string;
-    TesterID?: string;
-    Carrier_SN?: string;
-    DUT_SN?: string;
-    Error_Code?: string;
-    Error_Content?: string;
-    TesterIP?: string;
-    Project_Name?: string;
-    Script_Name?: string;
-    Serial_Number_Name?: string;
-    Tester_Duration?: string;
-    Tester_Start_Time?: string;
-    Tester_End_Time?: string;
-    Record_Time?: string;
 }
 
 export class ApiService {
